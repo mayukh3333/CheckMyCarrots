dc20436c2d02b62686fff5975f8e6bfe67bc8f65
Mayukh Dewan <mayukhdewan@gmail.com>
2020-09-08 18:20:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
+#import libraries
 import numpy as np
 from flask import Flask, request, render_template
 from flask_cors import CORS
 import os
-#from sklearn.externals import joblib
 import joblib
 import pickle
 import flask
@@ -13,18 +13,20 @@ import nltk
 import random
 nltk.download('punkt')
 
+#initialize the app
 app = Flask(__name__)
 CORS(app)
 app = flask.Flask(__name__,template_folder='templates')
 
+#loading the model
 with open('model.pk1','rb') as handle:
     model = pickle.load(handle)
-
 
 @app.route('/')
 def main():
     return render_template('index.html')
 
+#using NLP to parse the article
 @app.route('/predict',methods=['GET','POST'])
 def predict():
     url = request.get_data(as_text = True)[5:]
@@ -35,7 +37,7 @@ def predict():
     article.nlp()
     news = article.summary
     pred = model.predict([news])
-    return render_template('index.html',prediction_text='There is a {}% chance the article is "{}"'.format(random.randint(70,93)+round(random.random(),2),pred[0]))
+    return render_template('index.html',prediction_text='The article is "{}"'.format(pred[0]))
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT',5000))
